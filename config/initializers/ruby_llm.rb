0bd8a82a0c433e538359e69b35a5672d9a483ec1
Kurnia Muhamad <omenkzz@gmail.com>
2025-09-26 01:36:58 +0200

    Adding chat with AI

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,6 +1,9 @@
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV['OPENAI_API_KEY'] || Rails.application.credentials.dig(:openai_api_key)
+  #config.openai_api_key = ENV['OPENAI_API_KEY'] || Rails.application.credentials.dig(:openai_api_key)
   # config.default_model = "gpt-4.1-nano"
+  
+  config.openrouter_api_key = Rails.application.credentials.dig(:openrouter_api_key)
+  config.default_model = "deepseek/deepseek-chat-v3.1:free"
 
   # Use the new association-based acts_as API (recommended)
   config.use_new_acts_as = true
